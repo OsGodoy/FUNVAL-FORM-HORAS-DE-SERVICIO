653943c8d60f7b582b8ee05fb4b1c1c9b3c2ab4c
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-18 14:20:04 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-red-300 text-2xl">
+      Sphinx of black quartz, judge my vow.
+    </div>
+  );
+}
