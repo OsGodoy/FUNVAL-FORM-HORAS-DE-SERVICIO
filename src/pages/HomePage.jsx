6e750efd91d32f56962a65feef89ba0191204bd0
Roberto Castillo <roberto.castnj@gmail.com>
2025-10-21 14:51:53 -0600

    configuration of the home and adding the cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import Cards from "../components/shared/Cards";
+import React from 'react'
+import Cards from '../components/shared/Cards'
+import HeaderHome from '../components/Home-components/header-home-s'
+import Footer from '../components/Home-components/footer-home'
+import Courses from '../components/Home-components/courses'
 
 export default function HomePage() {
   return (
     <>
-      <Cards />
+      <HeaderHome />
+      <Courses />
+      <Footer />
     </>
-  );
+  )
 }
