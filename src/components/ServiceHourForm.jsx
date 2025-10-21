a67ab9184977ff6c20030153dfcc8f97d963b7d0
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-20 21:31:44 -0500

    Refactor ServiceHourForm components for improved styling and functionality - Updated color classes for better visibility - Changed input type from number to text in ServiceHourForm1 - Enhanced dropdowns for school and service selection in ServiceHourForm2

--- src/components/ServiceHourForm.jsx
+++ src/components/ServiceHourForm.jsx
@@ -20,16 +20,16 @@ export default function ServiceHourForm() {
               className={`${
                 toggleNext
                   ? "text-gray-400 font-normal"
-                  : "text-blue-600 font-semibold"
+                  : "text-blue-500 font-semibold"
               } `}
             >
               1
             </span>
-            /
+            <span className="text-gray-400">/</span>
             <span
               className={`${
                 toggleNext
-                  ? "text-blue-600 font-semibold"
+                  ? "text-blue-500 font-semibold"
                   : "text-gray-400 font-normal"
               } `}
             >
@@ -37,33 +37,48 @@ export default function ServiceHourForm() {
             </span>
           </h1>
           <form
-            className="text-gray-500 pt-4 w-full h-120 flex items-center justify-center"
+            className="text-gray-500 pt-4 w-full h-120 flex items-center justify-center relative"
             action=""
           >
-            <div className="w-full h-full gap-4 flex flex-col items-center justify-start">
-              {toggleNext ? <ServiceHourForm2 /> : <ServiceHourForm1 />}
+            <div
+              className={`w-full h-full gap-4 flex flex-col items-center justify-start absolute inset-0 duration-300
+              ${
+                toggleNext
+                  ? "opacity-0 pointer-events-none"
+                  : "opacity-100 pointer-events-auto"
+              }  
+              `}
+            >
+              <ServiceHourForm1 />
+            </div>
+            <div
+              className={`w-full h-full gap-4 flex flex-col items-center justify-start absolute inset-0 duration-300
+              ${
+                toggleNext
+                  ? "opacity-100 pointer-events-auto"
+                  : "opacity-0 pointer-events-none"
+              }
+              `}
+            >
+              <ServiceHourForm2 />
             </div>
           </form>
           <div className="w-full flex items-center justify-center">
             <button
               onClick={() => setToggleNext(!toggleNext)}
-              className={`w-24 border border-gray-400 rounded-full gap-1 flex items-center justify-between transition-all duration-400
-                ${toggleNext ? "pr-3.5" : "pl-2"}
+              className={`w-25 border border-gray-400 rounded-full gap-1 flex items-center justify-between hover:cursor-pointer
+                ${toggleNext ? "pl-4" : "pl-2.5"}
                 `}
             >
-              <p
-                className={`text-sm text-gray-400
-                ${toggleNext ? "order-2" : "order-1"}
-                `}
-              >
+              <p className={`text-sm text-gray-400`}>
                 {toggleNext ? "anterior" : "siguiente"}
               </p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className={`size-8 text-gray-500
-                  ${toggleNext ? "order-1 rotate-y-180" : "order-2"}
+                className={`size-8 text-gray-500 duration-500 
+                  ${toggleNext && "rotate-y-180"}
                   `}
               >
                 <path
--- src/components/ServiceHourForm2.jsx
+++ src/components/ServiceHourForm2.jsx
@@ -1,7 +1,22 @@
+import { Play } from "lucide-react";
 import React, { useState } from "react";
 
 export default function ServiceHourForm2() {
+  const [toggleSchool, setToggleSchool] = useState(false);
+  const [schoolSelected, setSchoolSelected] = useState("Selecciona tu escuela");
+  const [toggleServiceType, setToggleServiceType] = useState(false);
+  const [serviceSelected, setServiceSelected] = useState(
+    "Seleccionar tipo de servicio"
+  );
   const [fileName, setFileName] = useState("Ningún archivo seleccionado");
+
+  const handleSchoolChange = (e) => {
+    setSchoolSelected(e.target.textContent);
+  };
+
+  const handleServiceChange = (e) => {
+    setServiceSelected(e.target.textContent);
+  };
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -18,18 +33,146 @@ export default function ServiceHourForm2() {
         />
       </div>
       <div className="w-full flex flex-col items-center justify-center">
-        <p className="self-start text-sm">Escuela que esté cursando</p>
-        <input
-          className="text-blue-500 font-medium border text-sm w-full border-gray-400 p-2 outline-none"
-          type="number"
-        />
+        <p className="self-start text-sm">Escuela que estas cursando</p>
+        <div className="border border-gray-400 w-full h-9.5 px-3 flex items-center justify-center relative hover:cursor-pointer">
+          <div
+            className={`w-62 h-52 gap-2.5 bg-white p-4 pr-9 rounded-b-md border border-gray-400 flex flex-col items-start justify-start absolute top-0 z-10 origin-top duration-500
+                ${toggleSchool ? "scale-y-100" : "scale-y-0"}
+                `}
+          >
+            <ul className="w-full h-full text-blue-500 font-medium text-sm flex flex-col items-start justify-between">
+              <li
+                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
+                className="p-1 w-40 hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Escuela 1
+              </li>
+              <li
+                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
+                className="p-1 w-40 hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Escuela 2
+              </li>
+              <li
+                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
+                className="p-1 w-40 hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Escuela 3
+              </li>
+              <li
+                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
+                className="p-1 w-40 hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Escuela 4
+              </li>
+              <li
+                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
+                className="p-1 w-40 hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Escuela 5
+              </li>
+            </ul>
+          </div>
+          <div
+            onClick={() => {
+              setToggleSchool(!toggleSchool), setToggleServiceType(false);
+            }}
+            className="w-full flex items-center justify-between"
+          >
+            <p
+              className={`text-sm
+              ${
+                schoolSelected !== "Selecciona tu escuela"
+                  ? "text-gray-700"
+                  : "text-gray-400"
+              }
+              `}
+            >
+              {schoolSelected}
+            </p>
+            <Play
+              className={`text-gray-400 fill-gray-400 size-2.5 rotate-90 duration-500 z-20
+              ${toggleSchool && "rotate-270"}
+              `}
+            />
+          </div>
+        </div>
       </div>
       <div className="w-full flex flex-col items-center justify-center">
         <p className="self-start text-sm">Tipo de servicio</p>
-        <input
-          className="text-blue-500 font-medium border text-sm w-full border-gray-400 p-2 outline-none"
-          type="text"
-        />
+        <div className="border border-gray-400 w-full h-9.5 px-3 flex items-center justify-center relative hover:cursor-pointer">
+          <div
+            className={`w-62 h-52 gap-2.5 bg-white p-4 pr-9 rounded-b-md border border-gray-400 flex flex-col items-start justify-start absolute top-0 z-0 origin-top duration-500
+                ${toggleServiceType ? "scale-y-100" : "scale-y-0"}
+                `}
+          >
+            <ul className="w-full h-full text-blue-500 font-medium text-sm flex flex-col items-center justify-between">
+              <li
+                onClick={(e) => (
+                  handleServiceChange(e), setToggleServiceType(false)
+                )}
+                className="p-1 w-full hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Tipo de Servicio 1
+              </li>
+              <li
+                onClick={(e) => (
+                  handleServiceChange(e), setToggleServiceType(false)
+                )}
+                className="p-1 w-full hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Tipo de Servicio 2
+              </li>
+              <li
+                onClick={(e) => (
+                  handleServiceChange(e), setToggleServiceType(false)
+                )}
+                className="p-1 w-full hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Tipo de Servicio 3
+              </li>
+              <li
+                onClick={(e) => (
+                  handleServiceChange(e), setToggleServiceType(false)
+                )}
+                className="p-1 w-full hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Tipo de Servicio 4
+              </li>
+              <li
+                onClick={(e) => (
+                  handleServiceChange(e), setToggleServiceType(false)
+                )}
+                className="p-1 w-full hover:bg-gray-100 hover:cursor-pointer"
+              >
+                Tipo de Servicio 5
+              </li>
+            </ul>
+          </div>
+          <div
+            onClick={() => {
+              setToggleServiceType(!toggleServiceType), setToggleSchool(false);
+            }}
+            className="w-full flex items-center justify-between"
+          >
+            <p
+              className={`text-sm
+              ${
+                serviceSelected !== "Seleccionar tipo de servicio"
+                  ? "text-gray-700"
+                  : "text-gray-400"
+              }
+              `}
+            >
+              {serviceSelected}
+            </p>
+            <Play
+              className={`text-gray-400 fill-gray-400 size-2.5 rotate-90 duration-500 
+              ${toggleServiceType && "rotate-270"}
+              `}
+            />
+          </div>
+        </div>
       </div>
       <div className="w-full flex flex-col items-center justify-center">
         <p className="self-start text-sm">Cantidad de horas a registrar</p>
@@ -57,7 +200,7 @@ export default function ServiceHourForm2() {
           className={`text-xs mt-1
             ${
               fileName !== "Ningún archivo seleccionado"
-                ? "text-blue-400"
+                ? "text-blue-500"
                 : "text-rose-400"
             }
             `}
