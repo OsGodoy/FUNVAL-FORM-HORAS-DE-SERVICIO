f87fb1389647532fa7db781850d2fdd979c85b0c
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-22 08:09:20 -0500

    Refactor courses and service hour forms for improved structure and styling

--- src/components/ServiceHourForm2.jsx
+++ src/components/ServiceHourForm2.jsx
@@ -1,29 +1,16 @@
-import { Play } from "lucide-react";
+import { Info, Play } from "lucide-react";
 import React, { useState } from "react";
 
 export default function ServiceHourForm2() {
-  const [toggleSchool, setToggleSchool] = useState(false);
-  const [schoolSelected, setSchoolSelected] = useState("Selecciona tu escuela");
-  const [toggleServiceType, setToggleServiceType] = useState(false);
+  const [toggleInfo, setToggleInfo] = useState(false);
   const [serviceSelected, setServiceSelected] = useState(
     "Selecciona el tipo de servicio"
   );
-  const [toggleCourseLevel, setToggleCourseLevel] = useState(false);
-  const [courseLevelSelected, setCourseLevelSelected] = useState(
-    "Selecciona el nivel cursado"
-  );
-  const [fileName, setFileName] = useState("Ningún archivo seleccionado");
 
-  const handleSchoolChange = (e) => {
-    setSchoolSelected(e.target.textContent);
-  };
+  const [fileName, setFileName] = useState("Ningún archivo seleccionado");
 
   const handleServiceChange = (e) => {
     setServiceSelected(e.target.textContent);
-  };
-
-  const handleLevelChange = (e) => {
-    setCourseLevelSelected(e.target.textContent);
   };
 
   const handleFileChange = (e) => {
@@ -33,172 +20,109 @@ export default function ServiceHourForm2() {
 
   return (
     <>
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="self-start text-sm">Nivel del curso</p>
-        <div className="border border-gray-400 w-full h-9.5 px-3 flex items-center justify-center relative hover:cursor-pointer">
-          <div
-            className={`w-full h-52 gap-2.5 bg-white p-4 pr-9 rounded-b-md border border-gray-400 flex flex-col items-start justify-start absolute top-0 z-20 origin-top duration-500
-                ${toggleCourseLevel ? "scale-y-100" : "scale-y-0"}
-                `}
-          >
-            <ul className="w-full h-full text-blue-500 font-medium text-sm flex flex-col items-start justify-between">
-              <li
-                onClick={(e) => (
-                  handleLevelChange(e), setToggleCourseLevel(false)
-                )}
-                className="p-1 min-w-40 flex items-center justify-start hover:bg-gray-100 hover:cursor-pointer"
-              >
-                Nivel 1
-              </li>
-            </ul>
-          </div>
-          <div
-            onClick={() => {
-              setToggleCourseLevel(!toggleCourseLevel),
-                setToggleSchool(false),
-                setToggleServiceType(false);
-            }}
-            className="w-full flex items-center justify-between"
-          >
-            <p
-              className={`text-sm
-              ${
-                courseLevelSelected !== "Selecciona el nivel cursado"
-                  ? "text-gray-700"
-                  : "text-gray-400"
-              }
-              `}
-            >
-              {courseLevelSelected}
-            </p>
-            <Play
-              className={`text-gray-400 fill-gray-400 size-4.5 p-1 rotate-90 duration-500 z-50
-              ${toggleCourseLevel && "rotate-270"}
-              `}
-            />
-          </div>
+      <div className="w-full gap-3 flex flex-col sm:flex-row items-start justify-center">
+        <div className="w-full p-2 rounded-lg border-2 border-blue-500 flex items-center justify-center">
+          <input
+            className="text-gray-600 font-medium w-full sm:px-7 sm:h-20 outline-none placeholder:font-normal placeholder:text-center sm:text-center"
+            placeholder="Cantidad de horas a registrar"
+            type="text"
+            inputMode="numeric"
+            pattern="[0-9]*"
+          />
         </div>
-      </div>
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="self-start text-sm">Escuela que estas cursando</p>
-        <div className="border border-gray-400 w-full h-9.5 px-3 flex items-center justify-center relative hover:cursor-pointer">
-          <div
-            className={`w-full h-52 gap-2.5 bg-white p-4 pr-9 rounded-b-md border border-gray-400 flex flex-col items-start justify-start absolute top-0 z-10 origin-top duration-500
-                ${toggleSchool ? "scale-y-100" : "scale-y-0"}
-                `}
+        <div className="w-full flex flex-col items-center justify-center">
+          <label
+            htmlFor="file_input"
+            className="w-full sm:h-20 p-2 bg-blue-500 text-white rounded-lg cursor-pointer flex items-center justify-center"
           >
-            <ul className="w-full h-full text-blue-500 font-medium text-sm flex flex-col items-start justify-between">
-              <li
-                onClick={(e) => (handleSchoolChange(e), setToggleSchool(false))}
-                className="p-1 min-w-40 flex items-center justify-start hover:bg-gray-100 hover:cursor-pointer"
-              >
-                Escuela 1
-              </li>
-            </ul>
-          </div>
-          <div
-            onClick={() => {
-              setToggleSchool(!toggleSchool), setToggleServiceType(false);
-            }}
-            className="w-full flex items-center justify-between"
-          >
-            <p
-              className={`text-sm
-              ${
-                schoolSelected !== "Selecciona tu escuela"
-                  ? "text-gray-700"
-                  : "text-gray-400"
-              }
-              `}
-            >
-              {schoolSelected}
-            </p>
-            <Play
-              className={`text-gray-400 fill-gray-400 size-4.5 p-1 rotate-90 duration-500 z-10
-              ${toggleSchool && "rotate-270"}
-              `}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="self-start text-sm">Tipo de servicio</p>
-        <div className="border border-gray-400 w-full h-9.5 px-3 flex items-center justify-center relative hover:cursor-pointer">
-          <div
-            className={`w-full h-52 gap-2.5 bg-white p-4 pr-9 rounded-b-md border border-gray-400 flex flex-col items-start justify-start absolute top-0 z-0 origin-top duration-500
-                ${toggleServiceType ? "scale-y-100" : "scale-y-0"}
-                `}
-          >
-            <ul className="w-full h-full text-blue-500 font-medium text-sm flex flex-col items-start justify-between">
-              <li
-                onClick={(e) => (
-                  handleServiceChange(e), setToggleServiceType(false)
-                )}
-                className="p-1 min-w-40 flex items-center justify-start hover:bg-gray-100 hover:cursor-pointer"
-              >
-                Tipo de Servicio 1
-              </li>
-            </ul>
-          </div>
-          <div
-            onClick={() => {
-              setToggleServiceType(!toggleServiceType), setToggleSchool(false);
-            }}
-            className="w-full flex items-center justify-between"
-          >
-            <p
-              className={`text-sm
-              ${
-                serviceSelected !== "Selecciona el tipo de servicio"
-                  ? "text-gray-700"
-                  : "text-gray-400"
-              }
-              `}
-            >
-              {serviceSelected}
-            </p>
-            <Play
-              className={`text-gray-400 fill-gray-400 size-4.5 p-1 rotate-90 duration-500 z-0
-              ${toggleServiceType && "rotate-270"}
-              `}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="self-start text-sm">Cantidad de horas a registrar</p>
-        <input
-          className="text-blue-500 font-medium border text-sm w-full border-gray-400 p-2 outline-none"
-          type="text"
-        />
-      </div>
-      <div className="mt-6 w-full flex flex-col items-center justify-center">
-        <p className="text-sm">Adjuntar evidencia en formato .pdf</p>
-        <label
-          htmlFor="file_input"
-          className="mt-0.5 w-60 px-4 py-2 bg-blue-500 text-white rounded-lg cursor-pointer flex items-center justify-center"
-        >
-          Seleccionar archivo
-        </label>
+            Seleccionar archivo en .pdf
+          </label>
 
-        <input
-          id="file_input"
-          type="file"
-          className="hidden"
-          onChange={handleFileChange}
-        />
-        <p
-          className={`text-xs mt-1
+          <input
+            id="file_input"
+            type="file"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+          <p
+            className={`text-xs mt-1
             ${
               fileName !== "Ningún archivo seleccionado"
                 ? "text-blue-500"
                 : "text-rose-400"
             }
             `}
-        >
-          {fileName}
-        </p>
+          >
+            {fileName}
+          </p>
+        </div>
       </div>
+      <div className="w-full p-4 rounded-lg border-2 border-blue-500 gap-2 flex flex-col items-center justify-center">
+        <p className="text-blue-500 font-semibold">Tipo de servicio</p>
+        <ul className="grid grid-cols-1 gap-y-3">
+          <li
+            onClick={() => setToggleInfo(true)}
+            className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer"
+          >
+            Templo e Historia familiar, Indexacion
+          </li>
+          <li className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer">
+            Instructor
+          </li>
+          <li className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer">
+            Liderazgo
+          </li>
+          <li className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer">
+            Revision
+          </li>
+          <li className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer">
+            Asistencia al templo
+          </li>
+          <li className="text-blue-500 bg-blue-100 border-2 border-blue-500 rounded-full px-3 py-1.5 text-center leading-4.5 flex items-center justify-center hover:cursor-pointer">
+            Templo
+          </li>
+        </ul>
+      </div>
+      <div className="border-2 border-gray-400 w-full rounded-lg py-2 px-3 text-sm hidden sm:flex items-center justify-center">
+        <textarea className="w-full outline-none" name="" id="" placeholder="Ingrese una descripción del servicio..."></textarea>
+      </div>
+      <button className="w-full p-2 text-white bg-blue-500 rounded-lg cursor-pointer">
+        Registrar
+      </button>
+      <section
+        className={`bg-blue-950/80 h-full w-full absolute inset-0 rounded-lg p-4 flex items-center justify-center duration-500
+        ${
+          toggleInfo
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }
+        `}
+      >
+        <div className="w-full sm:w-100 h-80 bg-white rounded-lg p-4 flex flex-col items-center justify-center relative">
+          <h3 className="text-center text-blue-500 font-medium">
+            Templo e Historia familiar, Indexacion:
+          </h3>
+          <p className="text-gray-500 text-center">
+            "Indexacion de nombre en family search"
+          </p>
+          <Info className="text-gray-400 absolute top-4 right-4" />
+          <div className="w-full">
+            <button
+              type="button"
+              className="p-2 text-sm text-white bg-blue-500 rounded-lg absolute bottom-4 right-4"
+            >
+              Seleccionar
+            </button>
+            <button
+              type="button"
+              onClick={() => setToggleInfo(false)}
+              className="p-2 text-sm text-white bg-gray-400 rounded-lg absolute bottom-4 left-4"
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/ServiceHoursPage.jsx
+++ src/pages/ServiceHoursPage.jsx
@@ -3,9 +3,8 @@ import ServiceHourForm from "../components/ServiceHourForm";
 
 export default function ServiceHoursPage() {
   return (
-     <div className="h-full flex items-center justify-center">
-         <ServiceHourForm />
-      </div>
-    
+    <div className="py-2 flex items-center justify-center">
+      <ServiceHourForm />
+    </div>
   );
 }
