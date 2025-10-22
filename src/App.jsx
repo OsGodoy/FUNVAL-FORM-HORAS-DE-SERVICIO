95816e97269d301f0dc24e8b96d967b832fce1a5
Roberto Castillo <roberto.castnj@gmail.com>
2025-10-22 13:50:06 -0600

    Log in changes and data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import ServiceHoursPage from "./pages/ServiceHoursPage";
-import ServiceHourLayout from "./layouts/ServiceHourLayout";
-import RolesPage from "./pages/RolesPage";
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import ServiceHoursPage from './pages/ServiceHoursPage'
+import ServiceHourLayout from './layouts/ServiceHourLayout'
+import RolesPage from './pages/RolesPage'
+import { AuthProvider } from './contexts/Auth-context'
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
 
-        <Route element={<ServiceHourLayout />}>
-          <Route path="/roles" element={<RolesPage />} />
-          <Route path="/service-hours" element={<ServiceHoursPage />} />
-        </Route>
-      </Routes>
+          <Route element={<ServiceHourLayout />}>
+            <Route path="/roles" element={<RolesPage />} />
+            <Route path="/service-hours" element={<ServiceHoursPage />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </>
-  );
+  )
 }
